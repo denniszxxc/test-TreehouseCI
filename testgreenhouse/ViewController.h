cbccfd163e14e55d525a775e4f6c54868787150d
Dennis <dennis.li@zensis.com>
2015-10-07 10:30:13 +0800

    init dummy project

---
+++ testgreenhouse/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testgreenhouse
+//
+//  Created by Dennis on 6/10/15.
+//  Copyright (c) 2015 Zensis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ testgreenhouse/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  testgreenhouse
+//
+//  Created by Dennis on 6/10/15.
+//  Copyright (c) 2015 Zensis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
